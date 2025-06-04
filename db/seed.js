abb308f11ba29fb89a3dd50c4619f379afe75e47
Kai Wui Ye <kye5595@gmail.com>
2025-06-04 17:29:22 -0400

    database setup

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,8 @@
+import { faker } from "@faker-js/faker";
+
 import db from "#db/client";
+import { createFolder } from "#db/queries/folders";
+import { createFile } from "#db/queries/files";
 
 await db.connect();
 await seed();
@@ -7,4 +11,19 @@ console.log("🌱 Database seeded.");
 
 async function seed() {
   // TODO
+  for (let i = 0; i < 3; i++) {
+    const folder = {
+      name: faker.commerce.department(),
+    };
+    const createdFolder = await createFolder(folder);
+
+    for (let j = 0; j < 5; j++) {
+      const file = {
+        name: faker.system.fileName(),
+        size: faker.number.int({ min: 100, max: 5000 }),
+        folderId: createdFolder.id,
+      };
+      await createFile(file);
+    }
+  }
 }
